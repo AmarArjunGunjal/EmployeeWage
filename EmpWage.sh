ca14161ad38c195be93ed6ba6067007672692d90
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-08 11:48:56 +0530

    Added : Part Time Working Hour

--- EmpWage.sh
+++ EmpWage.sh
@@ -4,14 +4,15 @@ echo "Welcome To The Employee Wage"
 
 EMP_WAGE_PER_HR=20
 
-emp_working_hr=8
+emp_fullTime_working_hr=8
+emp_partTime_working_hr=4
 
 empStatus=$((RANDOM%2))
 
 if [ $empStatus -eq 1 ]
 then
 	echo "Present"
-	dailyWage=$(( $EMP_WAGE_PER_HR*$emp_working_hr ))
+	dailyWage=$(( $EMP_WAGE_PER_HR*$emp_fullTime_working_hr ))
 	echo "$dailyWage"
 else
 	echo "Absent"
