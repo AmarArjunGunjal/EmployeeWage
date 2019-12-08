07fcb83b24fd8c222fa8b8dc8e9745738adec7f0
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-08 12:06:44 +0530

    Added: Calculated Part Time Daily Wage

--- EmpWage.sh
+++ EmpWage.sh
@@ -3,17 +3,31 @@
 echo "Welcome To The Employee Wage"
 
 EMP_WAGE_PER_HR=20
+FULL_TIME=0
+PART_TIME=1
 
-emp_fullTime_working_hr=8
-emp_partTime_working_hr=4
 
-empStatus=$((RANDOM%2))
+empStatus=$((RANDOM%3))
 
-if [ $empStatus -eq 1 ]
-then
-	echo "Present"
-	dailyWage=$(( $EMP_WAGE_PER_HR*$emp_fullTime_working_hr ))
+case $empStatus in
+	$FULL_TIME )
+
+			emp_working_hr=8
+			dailyWage=$(($EMP_WAGE_PER_HR*$emp_working_hr))
+
+	;;
+
+	$PART_TIME )
+
+			emp_working_hr=4
+			dailyWage=$(($EMP_WAGE_PER_HR*$emp_working_hr))
+
+	;;
+
+	* )
+			echo "Absent"
+
+esac
+
 	echo "$dailyWage"
-else
-	echo "Absent"
-fi
+
