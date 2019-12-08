2ad1b1de7c5102c36b79118e40dced287423ddad
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-08 11:37:03 +0530

    Added : Calculated Daily Wage

--- EmpWage.sh
+++ EmpWage.sh
@@ -2,11 +2,17 @@
 
 echo "Welcome To The Employee Wage"
 
+EMP_WAGE_PER_HR=20
+
+emp_working_hr=8
+
 empStatus=$((RANDOM%2))
 
 if [ $empStatus -eq 1 ]
 then
 	echo "Present"
+	dailyWage=$(( $EMP_WAGE_PER_HR*$emp_working_hr ))
+	echo "$dailyWage"
 else
 	echo "Absent"
 fi
